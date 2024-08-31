f570d182c991bf9465e42e2518be164360973d3f
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-08-31 22:02:07 +0200

    get all items and get item by id

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const fastify = require('fastify')({ logger: false })
 
+const items = require('./Items')
+
 fastify.get('/items' , (req , reply ) => {
-    reply.send({test: "hello"})
+    reply.send({items})
 })
+
+fastify.get('/items/:id', (req , reply ) => {
+    const {id} = req.params
+
+    const item = items.find((item) => item.id === id)
+
+    reply.send({item})
+})
+
+
+
 
 const start = async () => {
     try {
